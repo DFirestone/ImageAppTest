19667219a1f1d65c3f7c5101c4693263391a30c1
DFirestone <drakonfirestone@gmail.com>
2017-03-19 12:17:17 +0100

    Init commit

---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageAppTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog opFile = new OpenFileDialog();
+            opFile.Title = "Select a Image";
+            opFile.Filter = "jpg files (*.jpg)|*.jpg|All Files (*.*)|*.*";
+
+            string appPath = Path.GetDirectoryName(Application.ExecutablePath) + @"\ProImages\";
+            if (Directory.Exists(appPath) == false)                                              // <---
+            {                                                                                    // <---
+                Directory.CreateDirectory(appPath);                                              // <---
+            }
+
+            if (opFile.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string iName = opFile.SafeFileName;   // <---
+                    string filepath = opFile.FileName;    // <---
+                    if (!File.Exists(filepath))
+                        File.Copy(filepath, appPath + iName); // <---
+                    Bitmap Image = new Bitmap(appPath + iName);
+                    pictureBox1.Image = Image;
+                }
+                catch (Exception exp)
+                {
+                    MessageBox.Show("Unable to open file " + exp.Message);
+                }
+            }
+            else
+            {
+                opFile.Dispose();
+            }
+        }
+    }
+}
